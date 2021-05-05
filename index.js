f3ee2761e0596bde474094a4ac6f7f23f89d42ed
ZacharyGopinath <54950453+ZacharyGopinath@users.noreply.github.com>
2021-05-05 11:40:40 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const factorial = (n) => {
+    if (n == 0) return 0
+    if (n == 1) {
+        return 1;
+    }
+    else {
+        return n * factorial(n - 1)
+    }
+}
+document.getElementById('factorialSubmitButton').addEventListener('click', () => {
+    const int = document.getElementById('exampleInputEmail1').value;
+    document.getElementById('outputInt').textContent = 'Output: ' + factorial(int);
+})
+$(document).ready(function () {
+    $('#modalButton').hide();
+    document.getElementById('modalButton').click();
+});
